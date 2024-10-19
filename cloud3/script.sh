02e6348ae76f8ceb18973b3192ddd49ee349bbb0
Alejandro Arias <josea.ariasp@uqvirtual.edu.co>
2024-10-19 14:34:44 -0500

    (UPDATE) funciona la clonación del repo

--- cloud3/script.sh
+++ cloud3/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Cargar variables de ambiente
+# Cargar variables de ambiente
 source .env
 
 # Función para clonar o actualizar el repositorio
@@ -19,27 +19,26 @@ clone_or_pull_repo() {
 # Clonar o actualizar el repositorio antes de iniciar la VM
 clone_or_pull_repo
 
-#crear disco
+# Crear disco
 VBoxManage createmedium disk --filename "$DISK_PATH" --size "$SIZE" --format VDI
 if [ -f "$DISK_NAME" ]; then
 	echo "Se ha creado el disco $NAME en la ruta $DISK_PATH"
 fi
 
-#adjuntar el disco duro virtual
+# Adjuntar el disco duro virtual
 VBoxManage storageattach "$VM_NAME" --storagectl "$CONTROLLER" --port $PORT --device $DEVICE --type hdd --medium "$DISK_PATH"
 
-#verificar que si se adjunto
+# Verificar que se adjuntó correctamente
 if [ $? -eq 0 ]; then
 	echo "Disco adjuntado correctamente"
 
-	#levantar la maquina
+	# Levantar la máquina virtual
 	VBoxManage startvm "$VM_NAME" --type gui
 
 	if [ $? -eq 0 ]; then
 		echo "VM levantada exitosamente"
-
 	else
-		echo "Error al levantar la vm"
+		echo "Error al levantar la VM"
 		exit 1
 	fi
 else
@@ -53,29 +52,29 @@ sleep 35
 # Realizar git pull después de que la VM esté arriba (significa que ya subió las credenciales)
 clone_or_pull_repo
 
-# Obtener la última carpeta modificada o agregada en el último commit
+# Obtener la última carpeta (basada en la IP)
 LAST_COMMIT=$(git log -1 --pretty=format:"%H") # Obtener el hash del último commit
-LAST_DIR=$(git diff-tree --no-commit-id --name-only -r "$LAST_COMMIT" | grep / | tail -n 1)
+LAST_IP_DIR=$(git diff-tree --no-commit-id --name-only -r "$LAST_COMMIT" | grep / | cut -d '/' -f 1 | tail -n 1)
 
-if [ -z "$LAST_DIR" ]; then
-	echo "No se encontró ninguna carpeta modificada en el último commit."
+if [ -z "$LAST_IP_DIR" ]; then
+	echo "No se encontró ninguna carpeta de IP modificada en el último commit."
 	exit 1
 fi
 
-# Leer la IP desde ip_address.txt en la carpeta más reciente
-IP_FILE="$CLONE_DIR/$LAST_DIR/ip_address.txt"
+# Definir las rutas a los archivos de la carpeta correspondiente a la IP
+IP_FILE="$CLONE_DIR/$LAST_IP_DIR/ip_address.txt"
+SSH_KEY_PATH="$CLONE_DIR/$LAST_IP_DIR/ssh_key"
 
+# Verificar que el archivo de la IP existe
 if [ ! -f "$IP_FILE" ]; then
-	echo "No se encontró el archivo ip_address.txt en $LAST_DIR"
+	echo "No se encontró el archivo ip_address.txt en $LAST_IP_DIR"
 	exit 1
 fi
 
-# Guardar la IP y rutas en variables
+# Guardar la IP y la ruta de la llave SSH
 IP_ADDRESS=$(cat "$IP_FILE")
-FOLDER_PATH="$CLONE_DIR/$LAST_DIR"
-SSH_KEY_PATH="$CLONE_DIR/$LAST_DIR/ssh_key"
 
 # Mostrar la información
-echo "Última carpeta modificada/agregada: $FOLDER_PATH"
+echo "Última carpeta (IP) modificada: $LAST_IP_DIR"
 echo "IP leída: $IP_ADDRESS"
 echo "Ruta de la llave SSH: $SSH_KEY_PATH"
